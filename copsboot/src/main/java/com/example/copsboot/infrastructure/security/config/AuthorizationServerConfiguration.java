c9c481a1bfb23fe51401b556c5f809a6fe186303
Nick Kapetanas <nkapetanas@outlook.com>
2019-09-17 21:01:08 +0300

    fixes to AuthorizationServer

--- copsboot/src/main/java/com/example/copsboot/infrastructure/security/config/AuthorizationServerConfiguration.java
+++ copsboot/src/main/java/com/example/copsboot/infrastructure/security/config/AuthorizationServerConfiguration.java
@@ -16,19 +16,23 @@ import static com.example.copsboot.infrastructure.security.config.ResourceServer
 
 
 /*
-* The authorization server is responsible for the authorization of the client application and the users
-*
-*/
+ * The authorization server is responsible for the authorization of the client application and the users
+ *
+ */
 
 @Configuration
-@EnableAuthorizationServer
 // The @EnableAuthorizationServer annotation ensures that you have an authorization server started
+@EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    // UserDetailsService is the contact point between your application and Spring Security.
+    /*
+     * UserDetailsService is the contact point between the application and Spring Security. You will have
+     * to create a class that implements this interface, which will use the previously created
+     * UserRepository to know what users you have in your application.
+     */
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -39,10 +43,11 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private TokenStore tokenStore;
 
-    // You obviously should not store user passwords in plain text. This example uses an implementation
-    // of PasswordEncoder to encrypt passwords when storing them in the database. PasswordEncoder hashes
-    // the password so that there is no way to recover the original password, even when the complete
-    // database leaks out.
+    /* You obviously should not store user passwords in plain text. This example uses an implementation
+     * of PasswordEncoder to encrypt passwords when storing them in the database. PasswordEncoder hashes
+     * the password so that there is no way to recover the original password, even when the complete
+     * database leaks out.
+     */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws
             Exception {
@@ -51,9 +56,11 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory() //  You define one client application that will be allowed to get an access token in memory. Since you
-       //  only have one mobile app for now, you don’t need anything more elaborate. You may define this in
-       //  the database if you want. You would then use clients.jdbc(dataSource).
+        /*
+         * only have one mobile app for now, you don’t need anything more elaborate. You may define this in
+         * the database if you want. You would then use clients.jdbc(dataSource).
+         */
+        clients.inMemory()
                 .withClient("copsboot-mobile-client") // hard-coded the client ID for authentication of the client application to copsboot-mobile-client
                 .authorizedGrantTypes("password", "refresh_token") // This has to be "password" to allow password flow using the defined client application. Also add "refresh_token" so that the client application can use the refresh token to get a new access token
                 .scopes("mobile_app") // The scopes allow you to define what "part" of the application is allowed by the received token. Let’s not do anything based on that value for now, so its exact value does not really matter
@@ -62,8 +69,7 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     }
 
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws
-            Exception {
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore)
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
