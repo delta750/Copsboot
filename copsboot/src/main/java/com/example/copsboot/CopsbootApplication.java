19917a576314510fd66365a6e1cda920a165eec1
Nick Kapetanas <nkapetanas@outlook.com>
2019-09-19 23:51:09 +0300

    fixed missing Bean declaration

--- copsboot/src/main/java/com/example/copsboot/CopsbootApplication.java
+++ copsboot/src/main/java/com/example/copsboot/CopsbootApplication.java
@@ -5,8 +5,10 @@ import com.example.orm.jpa.UniqueIdGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
@@ -23,6 +25,10 @@ public class CopsbootApplication {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}@Bean
+
+	public AuthenticationManager authenticationManager() {
+		return new OAuth2AuthenticationManager();
 	}
 
 	@Bean
